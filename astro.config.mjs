77caf460cfda02e5d6bdcbf1119560269d1baa7e
PROGRAMADOR EXTERNO <programador_externo@zermat.com.mx>
2025-06-27 11:44:53 -0600

    Page - Inicio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   integrations: [react()],
+   alias: {
+      "@": "./src",
+      "@components": "/src/components",
+      "@layouts": "/src/layouts",
+      "@pages": "/src/pages",
+      "@styles": "/src/styles",
+   },
+   vite: {
+      plugins: [tailwindcss()],
+   },
+   output: "static",
+   build: {
+      inlineStylesheets: "auto",
+   },
+   server: {
+      host: true,
+      port: 7001,
+   },
+});
