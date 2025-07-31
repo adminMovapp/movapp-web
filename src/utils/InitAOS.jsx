b2611a78f4c2d3490f543a554d83676a89bf065a
adminMovapp <admin@movapp.org>
2025-07-30 20:37:07 -0600

    feat: format code

--- src/utils/InitAOS.jsx
+++ src/utils/InitAOS.jsx
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const InitAOS = () => {
-   useEffect(() => {
-      AOS.init({
-         duration: 800,
-         once: true,
-         disable: () => window.innerWidth < 768, // ⛔ Desactiva AOS en móviles
-      });
-   }, []);
+    useEffect(() => {
+        AOS.init({
+            duration: 800,
+            once: true,
+            disable: () => window.innerWidth < 768, // ⛔ Desactiva AOS en móviles
+        });
+    }, []);
 
-   return null;
+    return null;
 };
 
 export default InitAOS;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,36 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   theme: {
-      extend: {
-         fontFamily: {
-            montserrat: ["Montserrat", "sans-serif"],
-         },
-         colors: {
-            debug: "#00ffff",
-            purple_mv: "#8149E2",
-            gray_mv: "#4A4A4A",
-            greylight_mv: "#767676",
-         },
-         keyframes: {
-            slideUp: {
-               "0%": { transform: "translateY(100%)" },
-               "100%": { transform: "translateY(0)" },
+    theme: {
+        extend: {
+            fontFamily: {
+                montserrat: ['Montserrat', 'sans-serif'],
             },
-            slideDown: {
-               "0%": { transform: "translateY(0)" },
-               "100%": { transform: "translateY(100%)" },
+            colors: {
+                debug: '#00ffff',
+                purple_mv: '#8149E2',
+                gray_mv: '#4A4A4A',
+                greylight_mv: '#767676',
             },
-         },
-         animation: {
-            slideUp: "slideUp 0.3s ease-out forwards",
-            slideDown: "slideDown 0.3s ease-in forwards",
-         },
-      },
-   },
-   content: [
-      "./src/**/*.{astro,html,js,jsx,ts,tsx}",
-      "./components/**/*.{astro,js,jsx,ts,tsx}",
-      "./layouts/**/*.{astro,js,jsx,ts,tsx}",
-   ],
-   plugins: [],
+            keyframes: {
+                slideUp: {
+                    '0%': { transform: 'translateY(100%)' },
+                    '100%': { transform: 'translateY(0)' },
+                },
+                slideDown: {
+                    '0%': { transform: 'translateY(0)' },
+                    '100%': { transform: 'translateY(100%)' },
+                },
+            },
+            animation: {
+                slideUp: 'slideUp 0.3s ease-out forwards',
+                slideDown: 'slideDown 0.3s ease-in forwards',
+            },
+        },
+    },
+    content: [
+        './src/**/*.{astro,html,js,jsx,ts,tsx}',
+        './components/**/*.{astro,js,jsx,ts,tsx}',
+        './layouts/**/*.{astro,js,jsx,ts,tsx}',
+    ],
+    plugins: [],
 };
